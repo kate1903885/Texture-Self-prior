51a3083216d9ca9906c11bdeafd5ae9559efd987
kate1903885 <2430120736@qq.com>
2025-07-18 11:38:51 +0800

    Add files via upload

---
+++ OptCuts-master/src/Types.hpp
@@ -0,0 +1,27 @@
+//
+//  Type.hpp
+//  OptCuts
+//
+//  Created by Minchen Li on 2/5/18.
+//
+
+#ifndef Types_hpp
+#define Types_hpp
+
+#include <cstdio>
+
+#define DIM 2
+
+//#define LINSYSSOLVER_USE_CHOLMOD
+//#define LINSYSSOLVER_USE_PARDISO
+
+namespace OptCuts {
+    enum MethodType {
+        MT_OPTCUTS,
+        MT_EBCUTS,
+        MT_OPTCUTS_NODUAL,
+        MT_DISTMIN
+    };
+}
+
+#endif /* Types_hpp */
